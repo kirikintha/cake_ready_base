86eba6346951a246429393f187cf9c36a986ea58
Paul Huntsberger <paul@303software.com>
2013-11-06 21:18:31 -0700

    Removing import and export items, users will need to make these on their own.

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
-
-SCHEMA='qns_sdk'
-
-TEST_SCHEMA=$SCHEMA"_test"
-
-#Remove old files.
-rm -rf $SCHEMA
-rm -rf $TEST_SCHEMA
-
-#Dump from Mongo.
-mongodump -v --db $SCHEMA --out install/
-mongodump -v --db $TEST_SCHEMA --out install/
-
-#Compress and clean up directories.
-cd install
-
-tar cvzf $SCHEMA.tar.gz $SCHEMA/
-tar cvzf $TEST_SCHEMA.tar.gz $TEST_SCHEMA/
-
-rm -rf $SCHEMA
-rm -rf $TEST_SCHEMA
+#@todo - your export here.
